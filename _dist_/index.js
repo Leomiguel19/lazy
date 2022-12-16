5a6293802e8f6431316c5a6c0c1235b73dd0350f
Leomiguel19 <Leomiguel19@users.noreply.github.com>
2022-12-16 03:33:54 +0000

    deploy: d5a9c94be4198751931dcac03eee98368a8ad5d1

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,31 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const maximum = 122;
+const minimum = 1;
+const random = () => Math.floor(Math.random()* (maximum-minimum) + minimum);
 
-console.log('Happy hacking :)')
+const createImageNode = () => {
+    const container = document.createElement("div");
+    container.className = "p-4";
+
+    const image = document.createElement("img");
+    image.className = "mx-auto";
+    image.width = "320";
+    image.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(image);
+
+    return container;
+}
+
+const newImage = createImageNode();
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector('button');
+
+//
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+}
+
+addButton.addEventListener("click", addImage);
+ 
