de097a41c4e033c4a23f0c721d550bab29a458e2
Leomiguel19 <Leomiguel19@users.noreply.github.com>
2022-12-21 03:54:37 +0000

    deploy: 97b09be3d1c25f5a5c154b570f060e6751d26522

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,3 +1,4 @@
+import h from '../_snowpack/pkg/hyperscript.js';
 import { registerImage } from "./lazy.js";
 
 const maximum = 122;
@@ -5,15 +6,20 @@ const minimum = 1;
 const random = () => Math.floor(Math.random()* (maximum-minimum) + minimum);
 
 const createImageNode = () => {
-    const container = document.createElement("div");
-    container.className = "p-4";
+    // const container = document.createElement("div");
+    // container.className = "p-4";
 
-    const image = document.createElement("img");
-    image.className = "mx-auto";
-    image.width = "320";
-    image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+    // const image = document.createElement("img");
+    // image.className = "mx-auto";
+    // image.width = "320";
+    // image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
 
-    container.appendChild(image);
+    const image = h('img.mx-auto', {
+        width: '320',
+        'data-src': `https://randomfox.ca/images/${random()}.jpg`,
+    });
+
+    const container = h("div.p-4.mt-3", image)
 
     return container;
 }
